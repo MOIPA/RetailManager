b6448b15f2aaad85453239b09c04d2ac8456b139
TR <fqy8323@dingtalk.com>
2021-04-23 14:21:50 +0800

    配置好x-admin和layui+jquery的框架，配置好前端选项卡，配置好单点登陆校验系统，配置服务器session，数据库设计中等待完成

--- Web/pages/baseURL.js
+++
@@ -1,9 +0,0 @@
-export default {
-    baseUrl: "http://localhost:9999",
-
-    userAdd: "/user/addUser",
-    userGetById: "/user/getUserById",
-    userGetByName: "/user/getUserByName",
-    login:"/user/login",
-    sessionCheck:"/session/check"
-}
