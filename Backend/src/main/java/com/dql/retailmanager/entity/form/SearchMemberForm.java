a505498862b8f0bbfda05958dba9854d93f035a5
TR <fqy8323@dingtalk.com>
2021-04-25 10:32:29 +0800

    update web module: add login and index

---
+++ Backend/src/main/java/com/dql/retailmanager/entity/form/SearchMemberForm.java
@@ -0,0 +1,12 @@
+package com.dql.retailmanager.entity.form;
+
+import com.dql.retailmanager.entity.page.PageRequest;
+import lombok.Data;
+
+@Data
+public class SearchMemberForm extends PageRequest {
+    /**
+     * user name
+     */
+    private String username;
+}
---
+++ Web/baseURL.js
@@ -0,0 +1,14 @@
+var baseUrl = "http://localhost:9999/api";
+export default {
+    // user
+    userAdd: baseUrl + "/user/addUser",
+    userGetById: baseUrl + "/user/getUserById",
+    userGetByName: baseUrl + "/user/getUserByName",
+    login: baseUrl + "/user/login",
+    // session 
+    sessionCheck: baseUrl + "/session/check",
+    // member
+    memberList: baseUrl + "/member/listMemberByPage",
+    addMember: baseUrl+"/member/addMember",
+    deleteMemberByIds: baseUrl +"/member/deleteMemberByIds"
+}
